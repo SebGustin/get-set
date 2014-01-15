4bbb2adb52523cae05411a2b32ba1cff29c6408c
Seb Gustin <seb@MacBook-Pro-de-Seb.local>
2014-01-14 20:21:01 -0500

    Connect classDiagramCtrl to app

--- js/controllers/classDiagramCtrl.js
+++ js/controllers/classDiagramCtrl.js
@@ -1,4 +1,35 @@
 getset.controller("ClassDiagramCtrl", function($scope) {
 	
+	$scope.model = [
+		{
+			"classname" : "Person",
+			"attributes": [
+				{
+					"visibility": "-", // private | protected | public | - | # | +
+					"name" : "firstname",
+					"multiplicity": 1,
+					"type": "string"
+				},
+				{
+					"visibility": "-", // private | protected | public | - | # | +
+					"name" : "lastname",
+					"multiplicity": 1,
+					"type": "string"
+				}
+			],
+			"methods": [
+				{
+					"visibility": "+",
+					"name" : "lastname",
+					"parameters": [],
+					"return": ""
+				}
+			]
+		}
+	];
+
+	$scope.drawModel = function() {
+		
+	};
     
 });
