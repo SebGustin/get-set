983d2998efeaeb95bd3a035018ea33f81b7f6440
Sébastien Gustin <seb.gustin@gmail.com>
2014-01-15 22:50:11 -0500

    add sets' method model and synchro ok with view

--- js/controllers/classDiagramCtrl.js
+++ js/controllers/classDiagramCtrl.js
@@ -4,26 +4,6 @@
  */
 getset.controller("ClassDiagramCtrl", function($scope) {
 	
-	function Attr() {
-		this.visibility = "";
-		this.name = "";
-		this.multiplicity = 0;
-		this.type = "";
-	}
-
-	function Method() {
-		this.visibility = "";
-		this.name = "";
-		this.parameters = "";
-		this.return = "";
-	}
-
-	function Model() {
-		this.className = "NewClass";
-		this.attributes = [];
-		this.methods = [];
-	}
-
 	$scope.models = [
 		{
 			"className" : "Person",
@@ -99,35 +79,84 @@ getset.controller("ClassDiagramCtrl", function($scope) {
 		newModel.attributes.push(new Attr());
 		newModel.methods.push(new Method());
 		$scope.models.push(newModel);
-		 
 	};
 
 	$scope.switchView = function(event) {
 		var $btn = $(event.currentTarget),
-			$parent = $btn.parents('.modelElement'),
-			$viewList = $parent.children('ul').find('li.viewList'),
-			$formList = $parent.children('ul').find('li.formList');
+			$modEl = $btn.parents('.modelElement'),
+			$viewList = $modEl.children('ul').find('li.viewList'),
+			$formList = $modEl.children('ul').find('li.formList');
 
 		if($viewList.hasClass('hidden')) {
 			$viewList.removeClass('hidden').addClass('fadeInDown').removeClass('fadeOutDown');
-			$formList.removeClass('fadeInUp').addClass('fadeOutUp').delay(300).addClass('hidden');
+			$formList.removeClass('fadeInUp').addClass('fadeOutUp').delay(10000).addClass('hidden');
 		} else {
-			$viewList.removeClass('fadeInDown').addClass('fadeOutDown').delay(300).addClass('hidden');
+			$viewList.removeClass('fadeInDown').addClass('fadeOutDown').delay(10000).addClass('hidden');
 			$formList.removeClass('hidden').addClass('fadeInUp').removeClass('fadeOutUp');
 		}
+
+		scrollToModel($modEl);
 	};
 
-	$scope.setReturn = function(method, event) {
-		var $target = $(event.currentTarget);
-		var ret = '';
+	$scope.setAttrName = function(attr, event) {
+		attr.name = getValueFor($(event.currentTarget));
+	};
 
-		if($target.context.localName === 'span')
-			ret = $target.text();
+	$scope.setAttrMultiplicity = function(attr, event) {
+		attr.multiplicity = getValueFor($(event.currentTarget));
+	};
+
+	$scope.setAttrType = function(attr, event) {
+		attr.type = getValueFor($(event.currentTarget));
+	};
+
+	$scope.setMethReturn = function(method, event) {
+		method.return = getValueFor($(event.currentTarget));
+	};
+
+	$scope.setMethName = function(method, event) {
+		method.name = getValueFor($(event.currentTarget));
+	};
+
+	$scope.setMethParameters = function(method, event) {
+		method.parameters = getValueFor($(event.currentTarget));
+	};
+
+	function getValueFor($el) {
+		if(getTagName($el) === 'span')
+			return $el.text();
 		else
-			ret = $target.val();
+			return $el.val();
+	}
 
-		method.return = ret;
-	};
-
-    
+	function getTagName($el) {
+		return $el.context.localName;
+	}
 });
+
+function Attr(vis, name, mult ,type) {
+	this.visibility   = (typeof vis == "undefined" ? "-" : vis);
+	this.name         = (typeof name == "undefined" ? "name" : name);
+	this.multiplicity = (typeof mult == "undefined" ? "multiplicity" : mult);
+	this.type         = (typeof type == "undefined" ? "type" : type);
+}
+
+function Method(vis, name, params, ret) {
+	this.visibility = (typeof vis == "undefined" ? "+" : vis);
+	this.name = (typeof name == "undefined" ? "name" : name);
+	this.params = (typeof params == "parameters" ? "" : params);
+	this.return = (typeof ret == "return" ? "" : ret);
+}
+
+function Model() {
+	this.className = "NewClass";
+	this.attributes = [];
+	this.methods = [];
+}
+
+function scrollToModel($el) {
+	console.log($el.parents('.model'));
+	$('html, body').animate({
+        scrollTop: $el.parents('.model').offset().top - 50
+    }, 300);
+}
