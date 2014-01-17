a9eee7e1913085584345251bd4025a0d06727b9f
Sébastien Gustin <seb.gustin@gmail.com>
2014-01-17 14:14:20 -0500

    add print btn on ClassDiagram, add markdown converter

--- js/controllers/dashboardCtrl.js
+++ js/controllers/dashboardCtrl.js
@@ -3,4 +3,11 @@ getset.controller("DashboardCtrl", function($scope) {
     $scope.model = {
         message: "dashboard"
     };
+
+    /* var converter = new Markdown.Converter(),
+     markdownToHtml = converter.makeHtml;
+*/
+    /*$.get("readme.md").success(function (data) {
+        console.log(markdownToHtml(data));
+    });*/
 });
